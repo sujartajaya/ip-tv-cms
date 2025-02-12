ca55da6b4afd115c9fc134cb00b7e300680a6f3d
Sujarta Jaya <sujarta.jaya@outlook.co.id>
2025-02-12 09:20:26 +0800

    my iptv

---
+++ laravel/src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function registration()
+    {
+        return view('user.register');
+    }
+
+    public function registerPost(Request $request)
+    {
+       $credentials = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email:dns','unique:users'],
+            'password' => ['required', 'string', 'min:8', 'same:confirm_password'],
+            'confirm_password' => ['min:8']
+        ]);
+        $credentials['password'] = bcrypt($credentials['password']);
+        $user = User::create($credentials);
+        return dd($user);
+    }
+
+    public function login()
+    {
+        return view('user.login');
+    }
+}
--- laravel/src/routes/web.php
+++ laravel/src/routes/web.php
@@ -3,14 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\TvController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     //return view('welcome');
-    $ip = request()->ip();
-    $hostname = request()->getHost();
-    $data['ip'] = $ip;
-    $data['host'] = $hostname;
-    return response()->json($data,200);
+    // $ip = request()->ip();
+    // $hostname = request()->getHost();
+    // $data['ip'] = $ip;
+    // $data['host'] = $hostname;
+    // return response()->json($data,200);
 });
 
 Route::get('/channels',[TvController::class,'index']);
+Route::get('/register', [AuthController::class, 'registration'])->name('register');
+Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+Route::get('/login',[AuthController::class,'login'])->name('login');
+Route::post('/login',[AuthController::class,'loginPost'])->name('login');
